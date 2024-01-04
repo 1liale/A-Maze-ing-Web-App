61b5a2c96d694d8da70dc488bf7df42e5bb12676
1liale <1alexli2002@gmail.com>
2024-01-04 10:39:45 -0500

    test

--- infrastructure/terraform/jenkins_master.tf
+++ infrastructure/terraform/jenkins_master.tf
@@ -57,7 +57,7 @@ resource "aws_instance" "jenkins_master" {
 
   root_block_device {
     volume_type           = "gp3"
-    volume_size           = 15
+    volume_size           = 30
     delete_on_termination = true
   }
 
