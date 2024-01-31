ae1a4f91b9c000bcdbc9fb0d763542fdc721ec12
1liale <1alexli2002@gmail.com>
2024-01-30 20:36:37 -0500

    added authentication for API using auth0, oauth2 compatible

--- backend/main.go
+++ backend/main.go
@@ -4,12 +4,17 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/1liale/maze-backend/handlers"
 	"github.com/1liale/maze-backend/middlewares"
+	"github.com/1liale/maze-backend/models"
 	"github.com/gin-contrib/cors"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
 	ginlogrus "github.com/toorop/gin-logrus"
@@ -31,11 +36,23 @@ func init() {
 	logger = middlewares.InitLogger()
 }
 
+type Product struct {
+	ID    int     `json:"id"`
+	Title string  `json:"title"`
+	Code  string  `json:"code"`
+	Price float32 `json:"price"`
+}
+
 func main() {
 	port := fmt.Sprintf(":%s", os.Getenv("API_PORT"))
 
 	// init gin server
 	router := gin.New()
+
+	// To store custom types in our cookies,
+	// we must first register them using gob.Register
+	gob.Register(map[string]interface{}{})
+	store := cookie.NewStore([]byte("secret"))
 
 	// middlewares
 	router.Use(
@@ -44,16 +61,46 @@ func main() {
 		middlewares.ErrorHandler(),
 		middlewares.PropDBEnv(db),
 		cors.Default(), // enable CORS for all origins with all HTTP requests allowed by default
+		sessions.Sessions("auth-session", store),
 	)
 
+	authenticator, err := models.NewAuth()
+	if err != nil {
+		logger.Fatalf("Failed to initialize the authenticator: %v", err)
+	}
+
+	// TODO: Remove when done testing
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.Data(http.StatusOK, "text/html; charset=utf-8", []byte(`
+		<html><div>
+			<h3>Auth0 Example</h3>
+			<a href="/login">SignIn</a>
+		</div></html>`))
+	})
+
+	// special routes (auth, system checks, etc.)
+	router.GET("/login", handlers.Login(authenticator))
+	router.GET("/callback", handlers.Callback(authenticator))
+	router.GET("/logout", handlers.Logout)
+	router.GET("/api-health", handlers.SystemCheck)
+
 	// require auth for requests that modify DB
-	auth := router.Group("/").Use()
+	auth := router.Group("/").Use(middlewares.IsAuthenticated)
 	{
 		// create maze record for user (update if user exists already)
 		auth.PUT("/maze/:user", handlers.SaveMaze)
 
 		// delete user and their records
 		auth.DELETE("/maze/:user", handlers.DeleteMaze)
+
+		auth.GET("/products", func(c *gin.Context) {
+			products := []Product{
+				{ID: 1, Title: "Product 1", Code: "p1", Price: 100.0},
+				{ID: 2, Title: "Product 2", Code: "p2", Price: 200.0},
+				{ID: 3, Title: "Product 3", Code: "p3", Price: 300.0},
+			}
+			c.JSON(http.StatusOK, products)
+		})
 	}
 
 	// get records belonging to user
@@ -67,9 +114,6 @@ func main() {
 
 	// solve an unknown maze and send back solution
 	router.POST("/maze/solve", handlers.SolveMaze)
-
-	// gets a system check on api health
-	auth.GET("/api-health", handlers.SystemCheck)
 
 	router.Run(port)
 }
