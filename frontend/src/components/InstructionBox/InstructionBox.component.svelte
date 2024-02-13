a92fcf162efef845264497ca39f1d0735d0620a0
1liale <1alexli2002@gmail.com>
2024-02-13 14:37:54 -0500

    updated game instructions

--- frontend/src/components/InstructionBox/InstructionBox.component.svelte
+++ frontend/src/components/InstructionBox/InstructionBox.component.svelte
@@ -2,7 +2,8 @@
   <div class="h-full variant-ghost rounded-lg p-3">
     <p class="dark:text-primary-500/70 text-surface-500/70">
       <strong>Instructions: </strong>Use 'wasd' or arrow keys to move and mouse to control the view
-      rotations.
+      rotations. Pts are awarded based on completion speed, maze difficulty (area), and number of
+      moves. Have fun!
     </p>
   </div>
 </div>
