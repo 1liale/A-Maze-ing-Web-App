12e8a9a405d64d39250e1103f30785b67e0084f9
1liale <1alexli2002@gmail.com>
2024-08-23 04:43:44 -0400

    cleaned up circleci pipeline config

---
+++ frontend/src/components/Buttons/ReturnButton.test.ts
@@ -0,0 +1,10 @@
+// src/components/ReturnButton.test.ts
+import { render, fireEvent, screen } from '@testing-library/svelte';
+import { describe, it, expect, vi } from 'vitest';
+import ReturnButton from './ReturnButton.component.svelte'; // Update the import path accordingly
+
+describe('ReturnButton', () => {
+  it('should call onClick when clicked', async () => {
+    
+  });
+});
